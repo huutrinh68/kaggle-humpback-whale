1e91f9fe316a38677a44405cfdfe59f6787a883b
Tran Quang Thien <thientquang@gmail.com>
2019-01-29 21:57:41 +0900

    Modify data.py

--- data.py
+++ data.py
@@ -1,24 +1,36 @@
 import pathlib
 import gc
 import cv2
+import random
+import torch
+import torchvision
 import numpy as np
 import pandas as pd
+import torchvision.datasets as dset
+import matplotlib.pyplot as plt
+import imgaug as ia
 
-from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
-from sklearn.preprocessing import MultiLabelBinarizer
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
+from PIL import ImageOps
 from torch.utils.data import Dataset
 from torchvision import transforms as T
 from imgaug import augmenters as iaa
 from collections import OrderedDict
+from scipy.ndimage import affine_transform
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator
 
 # local import
-from utils import get_multihot
-from sacred import Ingredient
+from utils import get_multihot, imshow, show_plot
+from sacred import Ingredient, Experiment
 from path import path_ingredient
-data_ingredient = Ingredient('data', ingredients=[path_ingredient])
+from metadata import meta_ingredient, create_p2size, create_p2h, \
+    create_h2ps, create_h2p, create_h2ws, create_w2hs, \
+    create_w2ts, read_raw_image, create_t2w
 
+data_ingredient = Ingredient('data', ingredients=[path_ingredient,
+                                                  meta_ingredient])
 iaa_dict = {'rot90' : iaa.Affine(rotate=90),
             'rot-90': iaa.Affine(rotate=-90),
             'rot180': iaa.Affine(rotate=180),
@@ -28,201 +40,211 @@ iaa_dict = {'rot90' : iaa.Affine(rotate=90),
             'fliplr': iaa.Fliplr(1),
             'noop'  : iaa.Noop()}
 
-
 @data_ingredient.config
 def cfg():
     image_size = 512            # image size
-    n_channels = 4              # num of channels
-    n_classes  = 28             # num of classes
     n_workers  = 4              # num of loader workers
-    batch_size = 40             # batch size
+    batch_size = 8             # batch size
     augment    = True           # train augment
     n_tta      = 3              # num of tta aug actions
-    n_fold     = 5              # n fold
+    anisotropy = 2.15
+    crop_margin= 0.01
     upsampling = True
-    aug_train  = ['noop', 'rot90', 'rot180', 'rot270', 'shear',
-                  'flipud', 'fliplr']                             # train aug actions
+    same_ratio = 0.5
+    # aug_train  = ['noop', 'rot90', 'rot180', 'rot270', 'shear',
+    #               'flipud', 'fliplr']                             # train aug actions
+    aug_train  = ['noop', 'fliplr', 'shear']
     aug_tta    = ['noop', 'flipud', 'fliplr', 'rot90', 'rot-90']  # tta aug actions
 
 
-@data_ingredient.capture
-def load_gen(data_df, mode, path):
-    if mode in ['train', 'val']:
-        return CellDataset(data_df, path['root'] + path['train_data'], mode='train')
-    if mode == 'test':
-        return CellDataset(data_df, path['root'] + path['test_data'], mode='test')
-    if mode == 'test-val':
-        return CellDataset(data_df, path['root'] + path['train_data'], mode='test')
-
-
-@data_ingredient.capture
-def load_loader(dataset, mode, batch_size, n_workers):
-    if mode == 'train':
-        return DataLoader(dataset, batch_size = batch_size, shuffle=True,
-                          pin_memory=True, num_workers=n_workers)
-    if mode == 'val':
-        return DataLoader(dataset, batch_size = batch_size, shuffle=False,
-                          pin_memory=True, num_workers=n_workers)
-    if mode in ['test', 'test-val']:
-        return DataLoader(dataset, batch_size = 1, shuffle=False,
-                          pin_memory=True, num_workers=n_workers)
-
-@data_ingredient.capture
-def create_loader(fold, n_fold, seed, debug, path, n_classes, upsampling):
-    train_data_df, val_data_df = get_train_val_df(fold, n_fold, seed, debug)
-
-    # Up sampling
-    train_data_df = up_sampling(train_data_df, upsampling)
-
-    # Create data loader
-    train_gen = load_gen(data_df=train_data_df, mode='train')
-    val_gen   = load_gen(data_df=val_data_df, mode='val')
-    train_loader = load_loader(train_gen, 'train')
-    val_loader   = load_loader(val_gen, 'val')
-    labels_count = count_labels(train_data_df)
-
-    return train_loader, val_loader, labels_count
-
-@data_ingredient.capture
-def create_test_loader(fold, n_fold, seed, debug, path):
-    _, val_data_df = get_train_val_df(fold, n_fold, seed, debug)
-    test_data_df = pd.read_csv(path['root'] + path['sample_submission'])
-    if debug: test_data_df = test_data_df[:100]
-    test_gen    = load_gen(data_df=test_data_df, mode='test')
-    test_loader = load_loader(test_gen, mode='test')
-    val_gen     = load_gen(data_df=val_data_df, mode='test-val')
-    val_loader  = load_loader(val_gen, mode='test-val')
-
-    return test_loader, val_loader
-
-@data_ingredient.capture
-def get_train_val_df(fold, n_fold, seed, debug, path, n_classes):
-    # Load csv file
-    kaggle_df   = pd.read_csv(path['root'] + path['kaggle_csv'])
-    external_df = pd.read_csv(path['root'] + path['external_csv'])
-    all_files   = pd.concat([kaggle_df, external_df])
-    if debug: all_files = all_files[:100]
-
-    # Get the fold-th split
-    y = get_multihot(targets=all_files.Target, n_classes=n_classes)
-    mskf = MultilabelStratifiedKFold(n_splits=n_fold, shuffle=True, random_state=seed)
-    for _fold, (train_index, val_index) in enumerate(mskf.split(all_files, y)):
-        if _fold == fold: break # Get the fold-th split
-    train_data_df = all_files.iloc[train_index].reset_index()
-    val_data_df = all_files.iloc[val_index].reset_index()
-
-    return train_data_df, val_data_df
-
-@data_ingredient.capture
-def up_sampling(train_data_df, upsampling):
-    # Upsampling
-    if upsampling:
-        train_df_orig = train_data_df.copy()
-        lows = [15,15,15,8,9,10,8,9,10,8,9,10,17,20,24,26,15,27,15,20,24,17,8,15,27,27,27]
-        for i in lows:
-            target = str(i)
-            indicies = train_df_orig.loc[train_df_orig['Target'] == target].index
-            train_data_df = pd.concat([train_data_df, train_df_orig.loc[indicies]], ignore_index=True)
-            indicies = train_df_orig.loc[train_df_orig['Target'].str.startswith(target+" ")].index
-            train_data_df = pd.concat([train_data_df, train_df_orig.loc[indicies]], ignore_index=True)
-            indicies = train_df_orig.loc[train_df_orig['Target'].str.endswith(" "+target)].index
-            train_data_df = pd.concat([train_data_df, train_df_orig.loc[indicies]], ignore_index=True)
-            indicies = train_df_orig.loc[train_df_orig['Target'].str.contains(" "+target+" ")].index
-            train_data_df = pd.concat([train_data_df, train_df_orig.loc[indicies]], ignore_index=True)
-        del train_df_orig
-        gc.collect()
-
-    return train_data_df
-
-
-def count_labels(train_data_df):
-    target = train_data_df.apply(lambda x: x['Target'].split(' '), axis=1)
-    y = target.tolist()
-    list_order = [str(i) for i in range(28)]
-    mlb = MultiLabelBinarizer(list_order)
-    y = mlb.fit_transform(y)
-    labels_count = OrderedDict()
-    count_classes = np.sum(y, axis=0)
-    for i, count in enumerate(count_classes):
-        labels_count[i] = count
-    del target, y
-    gc.collect()
-
-    return labels_count
-
-# ==============================
-# dataset class
-class CellDataset(Dataset):
+# ============== cropDataGenerator ========
+# =========================================
+class cropDataGenerator():
     @data_ingredient.capture
-    def __init__(self, images_df, data_path, mode, path, n_classes, n_channels,
-                 image_size, augment, n_tta, aug_train, aug_tta):
-        if not isinstance(data_path, pathlib.Path):
-            base_path = pathlib.Path(data_path)
-        self.images_df = images_df.copy()
-        self.n_classes  = n_classes
-        self.n_channels = n_channels
-        self.image_size = image_size
-        self.augment = augment
-        self.mode  = mode
-        self.n_tta = n_tta
-        self.images_df.Id = self.images_df.Id.apply(lambda x:base_path / x)
-        self.mlb = MultiLabelBinarizer(classes = np.arange(0, n_classes))
-        self.mlb.fit(np.arange(0, n_classes))
-        self.aug_train = aug_train
-        self.aug_tta = aug_tta
+    def __init__(self, path, aug_train, crop_margin,
+                 anisotropy, image_size):
+        TRAIN_DF = path['root'] + path['train_csv']
+        SUB_DF   = path['root'] + path['sample_submission']
+        BB_DF    = path['root'] + path['bbox']
+        self.tagged = dict([(p, w) for _, p, w in pd.read_csv(TRAIN_DF).to_records()])
+        self.submit = [p for _, p, _ in pd.read_csv(SUB_DF).to_records()]
+        self.join   = list(self.tagged.keys()) + self.submit
+        self.p2h    = create_p2h(self.join)
+        self.p2bb   = pd.read_csv(BB_DF).set_index("Image")
+        self.aug_train   = aug_train
+        self.crop_margin = crop_margin
+        self.anisotropy  = anisotropy
+        self.image_size  = image_size
 
-    def __len__(self):
-        return len(self.images_df)
-
-    def preprocess(self,X):
-        return T.Compose([T.ToPILImage(),
-                          T.ToTensor(),
-                          T.Normalize(mean=[0.5,], std=[0.5,])])(X).float()
-
-    def __getitem__(self,index):
-        X = self.read_images(index)
-        if 'Predicted' in self.images_df:
-            labels = self.images_df.iloc[index].Predicted
-        else:
-            labels = np.array(list(map(int, self.images_df.iloc[index].Target.split(' '))))
-        y  = np.eye(self.n_classes,dtype=np.float)[labels].sum(axis=0)
-
-        if self.mode == 'train' or self.n_tta == None:
-            if self.augment: X = self.train_augment(X)
-            return self.preprocess(X) ,y
-        else:
-            Xs = [self.preprocess(aug_image)
-                  for aug_image in self.tta_augment(X)]
-            return Xs, y
-
-    def read_images(self, index):
-        row = self.images_df.iloc[index]
-        filename = str(row.Id.absolute())
-        #use only rgb channels
-        if self.n_channels == 4:
-            images = np.zeros(shape=(self.image_size, self.image_size, 4))
-        else:
-            images = np.zeros(shape=(self.image_size, self.image_size, 3))
-        r = np.array(Image.open(filename+"_red.jpg"))
-        g = np.array(Image.open(filename+"_green.jpg"))
-        b = np.array(Image.open(filename+"_blue.jpg"))
-        y = np.array(Image.open(filename+"_yellow.jpg"))
-        images[:,:,0] = r.astype(np.uint8)
-        images[:,:,1] = g.astype(np.uint8)
-        images[:,:,2] = b.astype(np.uint8)
-        if self.n_channels == 4:
-            images[:,:,3] = y.astype(np.uint8)
-        images = images.astype(np.uint8)
-        return images
+        self.h2p              = create_h2p(None, None)
+        self.w2ts, self.train = create_w2ts(None)
+        self.datagen          = ImageDataGenerator(rotation_range=2,
+                                                    width_shift_range=1,
+                                                    height_shift_range=1,
+                                                    shear_range=2,
+                                                    horizontal_flip=True,
+                                                    vertical_flip=False,
+                                                    samplewise_std_normalization=True,
+                                                    fill_mode='nearest')
 
     @data_ingredient.capture
+    def read_cropped_image(self, p, augment=True):
+        """
+        @param p : the name of the picture to read
+        @param augment: True/False if data augmentation should be performed
+        @return a numpy array with the transformed image
+        """
+        # If an image id was given, convert to filename
+        if p in self.h2p:
+            p = self.h2p[p]
+        img = read_raw_image(p).convert('L')
+        img = np.asarray(img)
+        row = self.p2bb.loc[p]
+        x0, y0, x1, y1 = row['x0'], row['y0'], row['x1'], row['y1']
+
+        # Fix the bounding box
+        size_y, size_x = img.shape
+        dx = x1 - x0
+        dy = y1 - y0
+        x0 -= dx * self.crop_margin
+        x1 += dx * self.crop_margin + 1
+        y0 -= dy * self.crop_margin
+        y1 += dy * self.crop_margin + 1
+
+        # Crop
+        if x0 < 0: x0 = 0
+        if y0 < 0: y0 = 0
+        if x1 > size_x: x1 = size_x
+        if y1 > size_y: y1 = size_y
+
+        img = img[int(y0):int(y1), int(x0):int(x1)]
+
+        # Resize and normalize
+        y, x = img.shape
+        if x > y: ratio = self.image_size / x
+        else:     ratio = self.image_size / y
+        if int(x * ratio) != 0 and int(y * ratio) != 0:
+            img = cv2.resize(img, (int(x * ratio), int(y * ratio))).astype(float)
+            img -= np.mean(img, keepdims=True)
+            img += np.abs(np.min(img))
+            img /= np.std(img, keepdims=True)
+            img  = (img - np.min(img)) / (np.max(img) - np.min(img)) * 255.0
+
+        # Padding
+        y, x = img.shape
+        new_img = np.ones((self.image_size, self.image_size)) * 230.0
+        _x = int((self.image_size - x)/2)
+        _y = int((self.image_size - y)/2)
+        new_img[_y:_y+y,_x:_x+x] = img
+        new_img = new_img.reshape(self.image_size, self.image_size, 1)
+        # Agumentation
+        if augment:
+            new_img = self.train_augment(new_img)
+        return new_img.reshape(self.image_size, self.image_size, 1)
+
+
+    def read_for_training(self, p):
+        return self.read_cropped_image(p, augment=True)
+
+
+    def read_for_validation(self, p):
+        return self.read_cropped_image(p, augment=False)
+
+
     def train_augment(self, image):
-        aug = iaa.Sequential([iaa.OneOf([iaa_dict[action] for action in self.aug_train])],
-                       random_order=True)
-        image_aug = aug.augment_image(image)
+        image = image.reshape((1,) + image.shape)
+        image_aug = self.datagen.flow(image, batch_size=1)[0]
         return image_aug
 
+
+# ============== siameseDataset ==========
+# ========================================
+class SiameseDataset(Dataset):
     @data_ingredient.capture
-    def tta_augment(self, image):
-        augs = [iaa_dict[action] for action in self.aug_tta]
-        return [action.augment_image(image) for action in augs[:self.n_tta]]
+    def __init__(self, data_reader, mode, batch_size, same_ratio, path, image_size):
+        self.data_reader = data_reader
+        self.same_ratio  = same_ratio
+        self.batch_size  = batch_size
+        self.image_size  = image_size
+        self.mode        = mode
+        self.t2w         = create_t2w(None)
+        self.w2ts, self.train = create_w2ts(None)
+        self.t2i = {}
+        for i, t in enumerate(self.train):
+            self.t2i[t] = i
+
+    def __getitem__(self, index):
+        should_choose_same = random.uniform(0, 1)
+        if should_choose_same < self.same_ratio:
+            a = self.train[index]
+            b = self.get_match(index)
+            c = 1
+        else:
+            a = self.train[index]
+            b = self.get_unmatch(index)
+            c = 0
+
+        if self.mode == 'train':
+            a = self.data_reader.read_for_training(a).copy()
+            b = self.data_reader.read_for_training(b).copy()
+        else:
+            a = self.data_reader.read_for_validation(a).copy()
+            b = self.data_reader.read_for_validation(b).copy()
+
+        return self.preprocess(a), self.preprocess(b), c
+
+    def get_match(self, idx):
+        whale = self.t2w[self.train[idx]]
+        t = random.choice(self.w2ts[whale])
+        return t
+
+    def get_unmatch(self, idx):
+        whale = self.t2w[self.train[idx]]
+        while True:
+            t = random.choice(self.train)
+            if whale != self.t2w[t]:
+                return t
+
+    def __len__(self):
+        return len(list(self.train))
+
+    def preprocess(self,X):
+        return T.Compose([T.ToTensor()])(X).float()
+
+@data_ingredient.capture
+def create_loader_siamese(siameseDataset, batch_size, n_workers):
+    data_reader     = cropDataGenerator()
+    siamese_dataset = SiameseDataset(data_reader, 'train')
+
+    return DataLoader(siameseDataset, shuffle=True,
+                      num_workers=n_workers, batch_size=batch_size)
+
+
+# ============ TEST ===========
+ex = Experiment('tmp', ingredients=[data_ingredient,
+                                    meta_ingredient])
+
+@ex.main
+def main(data, path):
+    data_reader     = cropDataGenerator()
+    siamese_dataset = SiameseDataset(data_reader, 'train')
+    visual_dataloader = DataLoader(siamese_dataset,
+                                   shuffle=True,
+                                   num_workers=4,
+                                   batch_size=16)
+
+    image_size = data['image_size']
+    data_iter = iter(visual_dataloader)
+    example_batch = next(data_iter)
+
+    for i in range(8):
+        a = example_batch[0][i,:,:]
+        b = example_batch[1][i,:,:]
+        a = a.reshape((image_size, image_size))
+        b = b.reshape((image_size, image_size))
+        print(example_batch[2][i])
+        imshow(a)
+        imshow(b)
+
+if __name__ == '__main__':
+    ex.run_commandline()
