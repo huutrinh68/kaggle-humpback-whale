31050db5af3f2595ffb6a4f84c6e3f7f71de846f
Tran Quang Thien <thientquang@gmail.com>
2019-02-05 19:49:29 +0900

    Add test.py

--- test.py
+++ test.py
@@ -1,44 +1,65 @@
 import random
 import torch
 import warnings
-import logging
 import os
+import sys
+import json
 import numpy as np
 import pandas as pd
-
-from sklearn.metrics import f1_score
-from tqdm import tqdm
 from skeleton.tester import Tester
-
 from sacred import Experiment
 from sacred.observers import MongoObserver, FileStorageObserver
 from sacred.utils import apply_backspaces_and_linefeeds
 from sacred.commands import print_config
+from tqdm import tqdm
+warnings.filterwarnings('ignore')
 
 # local import
 from optimizer import optimizer_ingredient, load_optimizer
 from criterion import criterion_ingredient, load_loss, f1_macro_aggregator
 from model import model_ingredient, load_model
-from data import data_ingredient, create_test_loader
+from data import data_ingredient, create_feature_loader
 from path import path_ingredient, prepair_dir
-from utils import sigmoid
 
 ex = Experiment('Test', ingredients=[model_ingredient,      # model
-                                       data_ingredient,       # data
-                                       path_ingredient])       # path
-ex.observers.append(MongoObserver.create(db_name='human_protein'))
-ex.observers.append(FileStorageObserver.create('exp_logs/experiments'))
+                                     optimizer_ingredient,  # optimizer
+                                     data_ingredient,       # data
+                                     path_ingredient,       # path
+                                     criterion_ingredient]) # criterion
+
+# ex.observers.append(MongoObserver.create(db_name='humpback_whale'))
+# ex.observers.append(FileStorageObserver.create('exp_logs/experiments'))
 ex.captured_out_filter = apply_backspaces_and_linefeeds
+config_dict = None
 
 @ex.config
 def cfg():
-    code = None
-    seed=2050
-    threshold = 0.2
-    resume = True
+    max_epochs = None #dummy
+    resume = None #dummy
+    exp_id = None
+    threshold = 0.99
     debug = False
     comment = ''
-    find_threshold = True
+    tmp0, tmp1 = None, None
+
+@ex.named_config
+def reload():
+    exp_id = sys.argv[-1].split('=')[-1]
+    if exp_id == 'reload_cfg':
+        print('The id of the experiment is not inputted! Abort!')
+    elif not os.path.isfile('exp_logs/experiments/{}/config.json'.format(exp_id)):
+        print('exp_logs/experiments/{}/config.json'.format(exp_id),
+              'not found! Use current config.')
+    else:
+        for tmp0, tmp1 in get_config_dict(exp_id).items():
+            locals()[tmp0] = tmp1
+
+def get_config_dict(exp_id):
+    config_path = 'exp_logs/experiments/{}/config.json'.format(exp_id)
+    with open(config_path, 'r') as f:
+        json_str = f.read()
+    config_dict = json.loads(json_str)
+    return config_dict
 
 @ex.capture
 def init(_run, seed, path):
@@ -50,44 +71,46 @@ def init(_run, seed, path):
     torch.backends.cudnn.benchmark = True
     warnings.filterwarnings('ignore')
 
-    logging.info('='*50)
+    print('='*50)
     print_config(_run)
-    logging.info('='*50)
+    print('='*50)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    logging.info('Using {} gpus!'.format(torch.cuda.device_count()))
+    print('Using', torch.cuda.device_count(), 'gpus!')
 
     return device
 
 @ex.main
-def main(_log, code, data, path, seed, threshold, find_threshold, debug):
+def main(path):
+    # Model
     device = init()
-    fold_submissions = []
-    for fold in range(data['n_fold']):
-        if torch.cuda.device_count() > 1:
-            model = torch.nn.DataParallel(load_model()).to(device)
-        else:
-            model = load_model().to(device)
+    if torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(load_model()).to(device)
+    else:
+        model = load_model().to(device)
 
-    # Data
-    train_siamese_loader, val_siamese_loader = create_siamese_loader()
+    train_feature_loader, test_feature_loader = create_feature_loader()
+    data_iter = iter(train_feature_loader)
+    example_batch = next(data_iter)
 
-    # Loss function
-    loss_func = load_loss()
-
-    test_tester = Tester(
+    # Features generating
+    train_feature_gen = Tester(
         alchemistic_directory = path['root'] + path['exp_logs'] + 'checkpoints/',
-        code = code, fold=fold, model=model, test_dataloader=test_loader,
+        code = get_dir_name(),
+        model=model,
+        test_dataloader=train_feature_loader,
     )
+
 
 @ex.capture
 def get_dir_name(model, optimizer, data, path, criterion, seed, comment):
-    name = model['backbone']
+    name = model['model'] + '_' + model['backbone']
     name += '_' + optimizer['optimizer'] + '_' + str(optimizer['lr'])
     name += '_' + criterion['loss']
     name += '_' + str(seed)
     name += '_' + str(comment)
     print('Experiment code:', name)
     return name
+
 
 if __name__ == '__main__':
     ex.run_commandline()
