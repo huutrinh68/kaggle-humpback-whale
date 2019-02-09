701af4642956d70378959b66551db38c1b583265
Tran Quang Thien <thientquang@gmail.com>
2019-02-10 03:15:18 +0900

    Add a linear between in model, and also batchnorm1d and dropout

--- models/FineTune.py
+++ models/FineTune.py
@@ -30,50 +30,26 @@ from cnn_finetune import make_model
 #         return x
 
 class Embedding(nn.Module):
-    def __init__(self, in_dim, out_dim, dropout=None, normalized=True):
+    def __init__(self, in_dim, out_dim, dropout=0.5, normalized=True):
         super(Embedding, self).__init__()
-        self.bn = nn.BatchNorm2d(in_dim, eps=1e-5)
-        self.linear = nn.Linear(in_features=in_dim, out_features=out_dim)
+        self.bn1 = nn.BatchNorm1d(in_dim, eps=1e-5)
+        self.bn2 = nn.BatchNorm1d(512, eps=1e-5)
+        self.linear1 = nn.Linear(in_features=in_dim, out_features=512)
+        self.linear2 = nn.Linear(in_features=512, out_features=out_dim)
         self.dropout = dropout
-        self.normalized = normalized
 
     def forward(self, x):
-        # x = self.bn(x)
-        # x = F.relu(x, inplace=True)
-        if self.dropout is not None:
-            x = nn.Dropout(p=self.dropout)(x, inplace=True)
-        x = self.linear(x)
-        if self.normalized:
-            norm = x.norm(dim=1, p=2, keepdim=True)
-            x = x.div(norm.expand_as(x))
+        x = self.bn1(x)
+        x = nn.Dropout(p=self.dropout, inplace=True)(x)
+        x = self.linear1(x)
+        x = F.relu(x, inplace=True)
+        x = self.bn2(x)
+        x = nn.Dropout(p=self.dropout, inplace=True)(x)
+        x = self.linear2(x)
         return x
 
 def make_classifier(in_dim, out_dim):
     return Embedding(in_dim, out_dim, normalized=True)
-
-# class FineTune(nn.Module):
-#     def __init__(self, backbone, dim):
-#         super().__init__()
-#         backbone = make_model(
-#             model_name=backbone,
-#             pretrained=True,
-#             classifier_factory=make_classifier)
-#         )._features
-#         self.backbone = backbone
-#         self.feature_dims = 512
-#         self.dim = dim
-#         self.classifier = Embedding(in_dim  = self.feature_dims,
-#                                     out_dim = self.dim,
-#                                     normalized = True)
-
-#     def forward(self, input):
-#         return self.classifier(self.get_features(input))
-
-#     def get_features(self, x):
-#         x = self.backbone(x)
-#         dim = 1
-#         for d in x.shape[2:]: dim *= d
-#         return x.resize(*x.shape[:2], dim).max(-1)[0]
 
 def Fine_Tune(backbone='resnet18', dim=512, pretrained=True, model_path=None):
     model = make_model(model_name=backbone, pretrained=True, num_classes=dim,
