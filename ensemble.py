3f9e4069d2e3366070644e24fab43606ede47706
huutrinh68 <trinhsp89@gmail.com>
2019-02-21 23:43:18 +0900

    0.894

--- ensemble.py
+++ ensemble.py
@@ -4,7 +4,7 @@ import csv
 
 sub_files = [
                  "./input/submission_855.csv",
-                 "./input/submission_822.csv",
+                #  "./input/submission_822.csv",
                  "./input/submission_875.csv",
                  "./input/submission_842.csv",
                  "./input/submission_874.csv",
@@ -12,7 +12,7 @@ sub_files = [
 
 sub_weight = [           
                 3*0.855**2,
-                1*0.822**2,
+                # 1*0.822**2,
                 5*0.875**2,
                 2*0.842**2,
                 4*0.874**2,
@@ -48,3 +48,28 @@ for p, row in enumerate(sub[0]):
     tops_trgt = sorted(target_weight, key=target_weight.get, reverse=True)[:npt]
     writer.writerow([row1[Hlabel], " ".join(tops_trgt)])
 out.close()
+
+# my submission file
+df = pd.read_csv("./submission_ensem.csv")
+leak_df = pd.read_csv("./leaks.csv")
+
+leak_map = {}
+
+for idx, row in leak_df.iterrows():
+    leak_map[row["b_test_img"]] = row["b_label"]
+
+submission_list = []
+for idx, row in df.iterrows():
+    if row["Image"] in leak_map:
+        id_list = row["Id"].split(" ")
+        if id_list[0] != leak_map[row["Image"]]:
+            print(id_list[0], leak_map[row["Image"]])
+            print(id_list)
+        id_list[0] = leak_map[row["Image"]]
+        id_string = " ".join(id_list)
+    else:
+        id_string = row["Id"]
+    submission_list.append(id_string)
+df["Id"] = submission_list
+# modified output
+df.to_csv("final_submission.csv", index=False)
